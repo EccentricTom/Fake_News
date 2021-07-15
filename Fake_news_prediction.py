37bcd07281c08145f0094648dea43055dd9156ff
tom <tom@inmail.me>
2021-07-15 15:12:10 +0200

    Third Commit

--- Fake_news_prediction.py
+++ Fake_news_prediction.py
@@ -1,7 +1,6 @@
 # Import necessary Libraries
-import numpy as np
+from IPython.display import display
 import pandas as pd
-import itertools
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
@@ -10,18 +9,34 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 # load the data and check the shape
 df = pd.read_csv('D:\\Python_Projects\\Fake_News\\news.csv')
 print(df.shape)
+display(df.head())
 
 # Get the Labels
 labels = df['label']
 print(labels.head())
 
 # split into train and test sets
-x_train, x_test, y_train, y_test = train_test_split(df['text'], labels, test_size=0.2, random_state=1313)
+x_train, x_test, y_train, y_test = train_test_split(df['text'],
+                                                    labels,
+                                                    test_size=0.2,
+                                                    random_state=1313)
 
 # initialize a TfidVectorizer
 tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
 
 # Transform the data
 tfidf_train = tfidf_vectorizer.fit_transform(x_train)
-tfidf_test = tfidf_vectorizer.fit_transform(x_test)
+tfidf_test = tfidf_vectorizer.transform(x_test)
 
+# Initialize a Passive Aggressive Classifier
+
+pac = PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train, y_train)
+
+# Predict on the test set and check accuracy
+y_pred = pac.predict(tfidf_test)
+score = accuracy_score(y_test, y_pred)
+print(f'Accuracy:{round(score*100, 2)}%')
+
+# Create a confusion matrix to check TP and FN rate
+display(confusion_matrix(y_test, y_pred, labels=['FAKE', 'REAL']))
